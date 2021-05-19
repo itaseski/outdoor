a4cdcfc187d7488384135cb59b42bf1a6d7896cf
itaseski <itaseski13@gmail.com>
2021-05-19 22:58:38 +0200

    first view

--- mariovo/urls.py
+++ mariovo/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from map import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("map.urls")),
     path('admin/', admin.site.urls),
 ]
