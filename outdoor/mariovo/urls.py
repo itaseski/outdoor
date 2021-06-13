290f1104bd3d2ed2ab43911e440b970743f98c03
itaseski <itaseski13@gmail.com>
2021-06-13 16:35:02 +0200

    del nature app

--- outdoor/mariovo/urls.py
+++ outdoor/mariovo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name = 'nature'
+app_name = 'mariovo'
 urlpatterns = [
     path('', views.index, name="index"),
 ]
--- outdoor/mariovo/views.py
+++ outdoor/mariovo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'nature/index.html')
+    return render(request, 'mariovo/index.html')
